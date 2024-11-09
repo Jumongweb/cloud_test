6bf3b332d3d671f840e0feb10a35aa330ab94608
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-11-09 15:05:37 +0100

    test controller

--- src/main/java/com/jumong/cloud_test/controller/UserController.java
+++ src/main/java/com/jumong/cloud_test/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.jumong.cloud_test.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, Jumong! /nThis is a test application for Jumong Cloud";
+    }
 
 }
